78222d2109411667a6d8844863beb08c9d5c0dc4
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2024-05-28 11:11:05 +0700

    alamat cabang

--- application/views/tentang/daftar_cabang.php
+++ application/views/tentang/daftar_cabang.php
@@ -11,27 +11,119 @@
     <section class="tp-choose-area position-relative pt-120">
         <div class="container">
             <div class="row justify-content-between">
-                <div class="col-xl-12">
+                <div class="col-xl-6 col-md-6">
                     <div class="row">
-                        <?php foreach($cbng as $row) { ?>
-                        <div class="col-sm-3">
+                        <div class="col-sm-6">
                             <div class="tp-choose-timeline ml-15 mb-30">
                                 <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp" data-wow-delay=".4s">
                                     <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
-                                            href=""><?= $row->nm_cbng?></a></h4>
-                                    <p><?= $row->alamat?></p>
-                                    <?php if($row->map_google != NULL) { { ?>
+                                            href="https://goo.gl/maps/ZYARECmPDmT9e4ALA">Cabang Cangkrang (Khusus
+                                            Ikhwan)</a></h4>
+                                    <p>Jl. Carangpulang. Gg. Masjid Alhuda I, Kp. Cangkrang RT.02 RW.02, Desa
+                                        Cikarawang, Kec. Dramaga, Kab. Bogor, Jawa Barat</p>
                                     <div class="maps btn btn-costum">
-                                        <a href="<?= $row->map_google?>">Lokasi Maps</a>
+                                        <a href="https://goo.gl/maps/ZYARECmPDmT9e4ALA">Lihat Lokasi Maps</a>
                                     </div>
-                                    <?php }} ?>
+                                </div>
+                                <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp" data-wow-delay=".6s">
+                                    <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                            href="https://goo.gl/maps/BnNAdPMRgqHfR5y98">Cabang Citeureup</a></h4>
+                                    <p>Jl. Pahlawan RT.06 RW.04, Desa Senja, Kec. Citeureup, Kab. Bogor, Jawa Barat</p>
+                                    <div class="maps btn btn-costum">
+                                        <a href="https://goo.gl/maps/BnNAdPMRgqHfR5y98">Lihat Lokasi Maps</a>
+                                    </div>
+                                </div>
+                                <div class="tp-choose-timeline-single pl-35 wow fadeInUp" data-wow-delay=".8s">
+                                    <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                            href="https://goo.gl/maps/HBGckrtdiRL1pJXK7">Cabang Citayam</a></h4>
+                                    <p>Villa Pabuaran Indah, Jl. Pradana Raya II, Desa Pabuaran, Kec. Bojong Gede, Kab.
+                                        Bogor</p>
+                                    <div class="maps btn btn-costum">
+                                        <a href="https://goo.gl/maps/HBGckrtdiRL1pJXK7">Lihat Lokasi Maps</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <?php }; ?>
+                        <div class="col-sm-6">
+                            <div class="tp-choose-timeline ml-15 mb-30">
+                                <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp" data-wow-delay="1s">
+                                    <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                            href="https://goo.gl/maps/FCWjmfzuZHbVNRDYA">Cabang Dramaga (Khusus Boarding
+                                            & Institute)</a>
+                                    </h4>
+                                    <p>Jl. Batu Hulung No.54, RT.02 RW.01, Kelurahan Margajaya, Kec. Bogor Barat, Kota
+                                        Bogor</p>
+                                    <div class="maps btn btn-costum">
+                                        <a href="https://goo.gl/maps/FCWjmfzuZHbVNRDYA">Lihat Lokasi Maps</a>
+                                    </div>
+                                </div>
+                                <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp" data-wow-delay="1.2s">
+                                    <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                            href="#">Cabang Jakarta Pusat</a></h4>
+                                    <p>Jl. Rawa Selatan V. Gg. Buntu No.30, RT.17 RW.04, Kec. JoharBaru, Jakarta Pusat
+                                    </p>
+                                </div>
+                                <div class="tp-choose-timeline-single pl-35 wow fadeInUp" data-wow-delay="1.4s">
+                                    <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                            href="#">Cabang Jember Jawa Timur</a></h4>
+                                    <p>Jl. Riau Gg. 1001 No. 106 (Batu Alam), Krajan Barat, Kec. Sumbersari, Kab.
+                                        Jember, Jawa Timur</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="tp-choose z-index">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="tp-choose-timeline ml-15 mb-30">
+                                    <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp"
+                                        data-wow-delay=".4s">
+                                        <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                                href="#">Cabang Pontianak, Kalimantan Barat</a></h4>
+                                        <p>Jalan Haji Rais Abdul Rachman, Komplek Hasia Permai No 15 A, Kec. Pontianak
+                                            Barat, Kota Pontianak, 78114</p>
+                                    </div>
+                                    <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp"
+                                        data-wow-delay=".6s">
+                                        <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                                href="#">Cabang Riau</a></h4>
+                                        <p>Jl. Raya PekanBaru, Taluk Kuantan KM.32, Sungai Pagar, Kec. Kampar Kiri
+                                            Hilir, Kab. Kampar, Riau</p>
+                                    </div>
+                                    <div class="tp-choose-timeline-single pl-35 wow fadeInUp" data-wow-delay=".8s">
+                                        <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                                href="#">Cabang Jambi</a></h4>
+                                        <p>Jl. Untung Suropati Irg. Darma Bakti No. 15 RT.46 Kec. Jelutung Kota Jambi
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="tp-choose-timeline ml-15 mb-30">
+                                    <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp"
+                                        data-wow-delay=".4s">
+                                        <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                                href="#">Cabang Medan, Sumatera Utara</a></h4>
+                                        <p>Dusun I, Desa Citaman Jernih, Kec.Perbaungan, Kab. Serdang Begadai, Sumatera
+                                            Utara</p>
+                                    </div>
+                                    <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp"
+                                        data-wow-delay=".6s">
+                                        <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                                href="#">Cabang Bukittinggi, Sumatera Barat</a></h4>
+                                        <p>Jl. Bypass (Simpang lembaga/simpang lapas, masuk sekitar 200 meter sebelah
+                                            kiri). Kampung Guci, Desa Lambah Tengah, IV Ankat,Kec. Ampek Angkek,
+                                            Kab.Agam, Sumatera Barat</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
     </section>
 </main>
 <!-- Choose area end here -->
