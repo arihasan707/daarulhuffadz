25cbcae23dd30e96a0ba45f197585cb06aa41e67
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2024-05-28 10:40:49 +0700

    alamat cabang

--- application/views/tentang/daftar_cabang.php
+++ application/views/tentang/daftar_cabang.php
@@ -8,24 +8,30 @@
     </section>
     <!-- Choose area start here -->
 
-    <section class="tp-choose-area position-relative pt-30 pb-30">
+    <section class="tp-choose-area position-relative pt-120">
         <div class="container">
-            <div class="row">
-                <?php foreach($cbng as $row) { ?>
-                <div class="col-lg-3 mb-4">
-                    <div class=" wow fadeInUp" data-wow-delay=".4s">
-                        <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><?= $row->nm_cbng?></h4>
-                        <p><?= $row->alamat?></p>
-                        <?php if($row->map_google != NULL) { { ?>
-                        <div class="maps btn btn-costum">
-                            <a href="<?= $row->map_google?>">Lokasi Maps</a>
+            <div class="row justify-content-between">
+                <div class="col-xl-12">
+                    <div class="row">
+                        <?php foreach($cbng as $row) { ?>
+                        <div class="col-sm-3">
+                            <div class="tp-choose-timeline ml-15 mb-30">
+                                <div class="tp-choose-timeline-single mb-55 pl-35 wow fadeInUp" data-wow-delay=".4s">
+                                    <h4 class="tp-choose-timeline-single-title mb-15 hover-theme-color"><a
+                                            href=""><?= $row->nm_cbng?></a></h4>
+                                    <p><?= $row->alamat?></p>
+                                    <?php if($row->map_google != NULL) { { ?>
+                                    <div class="maps btn btn-costum">
+                                        <a href="<?= $row->map_google?>">Lokasi Maps</a>
+                                    </div>
+                                    <?php }} ?>
+                                </div>
+                            </div>
                         </div>
-                        <?php }} ?>
+                        <?php }; ?>
                     </div>
                 </div>
-                <?php }; ?>
             </div>
-        </div>
     </section>
 </main>
 <!-- Choose area end here -->
