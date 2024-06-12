49a5a77decbcc14e5fdd77293a7338ef36c78f2d
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2024-06-12 13:48:51 +0700

    waktu 24 jam pendaftaran sukses

--- application/views/program/v_sukses.php
+++ application/views/program/v_sukses.php
@@ -1,3 +1,7 @@
+<?php
+$date = date_create($tempdata['created_at']);
+?>
+
 <div class="flash-data" data-flashdata="<?= $this->session->flashdata('sukses') ?>"></div>
 <main class="bg-gray-light">
     <?php
@@ -118,7 +122,7 @@
                     <div class="col-lg-6 col-xl-6">
                         <div class="card sukses-bayar text-center">
                             <h4 class="text-center pt-3">Pendaftaran Berhasil!</h4>
-                            <p><?= date_format($tempdata['created_at'], "Y-m-d H:i:s") ?> WIB</p>
+                            <p><?= date_format($date, "Y-m-d H:i:s") ?> WIB</p>
                             <div class="col-lg-12 d-flex justify-content-center">
                                 <dotlottie-player src="https://lottie.host/eba15ced-8977-429f-8cb8-65f15c0785ab/7GanDEl2to.json" background="transparent" speed="1" style="width: 130px; height: 130px" direction="1" loop autoplay></dotlottie-player>
                             </div>
